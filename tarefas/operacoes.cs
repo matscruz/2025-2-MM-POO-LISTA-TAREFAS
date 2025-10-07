ebcd407a2f3620c6763a63b9a165913cb370dee3
Matias Scruz <144630285+matscruz@users.noreply.github.com>
2025-10-07 00:18:44 +0000

    feat: Tarefa

---
+++ tarefas/operacoes.cs
@@ -0,0 +1,20 @@
+public class Operacoes
+{
+    public int id { get; set; }
+    {
+        return 0;
+    }
+
+    public Tarefa Buscar {int Id }
+    {
+    return null;
+}
+
+publc List<Tarefa> Listar()
+     {
+        return Array.Emply<Tarefa>();
+     }
+
+}   public.void Alterar (Tarefa Tarefa)
+
+  public void Excluir (Index id)
---
+++ tarefas/tarefa.cs
@@ -0,0 +1,10 @@
+public class Tarefa
+{
+    public Id Int { get; set; }
+    public Nome String { get; set; }
+    public Descricao String { get; set; }
+    public dataCriacao DateTime { get; set; }
+    public Status Int { get; set; }
+    public dataExecucao DateTime { get; set; }
+    
+}
